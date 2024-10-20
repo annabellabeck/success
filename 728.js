0d54dc106ef0517a4d3dffe474d20cf33b5475e0
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-20 18:42:58 +0700

    Update 728.js

--- 728.js
+++ 728.js
@@ -1,18 +1,24 @@
-(function() {
+// Konfigurasi iklan
+var atOptions = {
+		'key' : '32b9e14e3431ab388585fc9a9c85fea2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+};
+
+// Fungsi untuk memuat iklan
+function loadAd() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '//servicefigured.com/32b9e14e3431ab388585fc9a9c85fea2/invoke.js';
-    script.async = true;
+    script.src = '//servicefigured.com/' + atOptions.key + '/invoke.js';
+    document.body.appendChild(script);
+}
 
-    var atOptions = {
-        'key': '32b9e14e3431ab388585fc9a9c85fea2',
-        'format': 'iframe',
-        'height': 90,
-        'width': 728,
-        'params': {}
-    };
+// Panggil fungsi loadAd setelah halaman selesai dimuat
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    loadAd();
+} else {
+    document.addEventListener('DOMContentLoaded', loadAd);
+}
 
-    window.atOptions = atOptions;
-
-    document.head.appendChild(script);
-})();
