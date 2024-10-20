88e6ae139353f4e71190f14480f9d9d246bac873
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-20 18:41:27 +0700

    Update 300.js

--- 300.js
+++ 300.js
@@ -1,18 +1,23 @@
-(function() {
+// Konfigurasi iklan
+var atOptions = {
+		'key' : '0588635e143b313b5e3e078a470bf3ce',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+};
+
+// Fungsi untuk memuat iklan
+function loadAd() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '//servicefigured.com/0588635e143b313b5e3e078a470bf3ce/invoke.js';
-    script.async = true;
+    script.src = '//servicefigured.com/' + atOptions.key + '/invoke.js';
+    document.body.appendChild(script);
+}
 
-    var atOptions = {
-        'key': '0588635e143b313b5e3e078a470bf3ce',
-        'format': 'iframe',
-        'height': 250,
-        'width': 300,
-        'params': {}
-    };
-
-    window.atOptions = atOptions;
-
-    document.head.appendChild(script);
-})();
+// Panggil fungsi loadAd setelah halaman selesai dimuat
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    loadAd();
+} else {
+    document.addEventListener('DOMContentLoaded', loadAd);
+}
