d607f26a765bcb097658a35e446a6e4ec168da31
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 00:14:22 +0700

    Update 468.js

--- 468.js
+++ 468.js
@@ -1,23 +1,10 @@
-// Konfigurasi iklan
-var atOptions = {
+<script type="text/javascript">
+	atOptions = {
 		'key' : '01ba7007dc7492889adb28d4fa3ef801',
 		'format' : 'iframe',
 		'height' : 60,
 		'width' : 468,
 		'params' : {}
-};
-
-// Fungsi untuk memuat iklan
-function loadAd() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//servicefigured.com/' + atOptions.key + '/invoke.js';
-    document.body.appendChild(script);
-}
-
-// Panggil fungsi loadAd setelah halaman selesai dimuat
-if (document.readyState === 'complete' || document.readyState === 'interactive') {
-    loadAd();
-} else {
-    document.addEventListener('DOMContentLoaded', loadAd);
-}
+	};
+</script>
+<script type="text/javascript" src="//servicefigured.com/01ba7007dc7492889adb28d4fa3ef801/invoke.js"></script>
