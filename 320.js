7ce81ba1c7c94d13a8a3fc7b32678b843edfe814
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-20 17:17:54 +0700

    Update 320.js

--- 320.js
+++ 320.js
@@ -1,22 +1,23 @@
-(function() {
-    // Konfigurasi iklan
-    var adConfig = {
-        'key': 'cf3eb3eb6529351b0cc27bcb6fe72798',
-        'format': 'iframe',
-        'height': 50,
-        'width': 320,
-        'params': {}
-    };
+// Konfigurasi iklan
+var atOptions = {
+    'key' : 'cf3eb3eb6529351b0cc27bcb6fe72798',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+};
 
-    // Buat elemen script untuk atOptions
-    var atOptionsScript = document.createElement('script');
-    atOptionsScript.type = 'text/javascript';
-    atOptionsScript.text = 'var atOptions = ' + JSON.stringify(adConfig) + ';';
-    document.head.appendChild(atOptionsScript);
+// Fungsi untuk memuat iklan
+function loadAd() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//servicefigured.com/' + atOptions.key + '/invoke.js';
+    document.body.appendChild(script);
+}
 
-    // Buat elemen script untuk memuat iklan
-    var adScript = document.createElement('script');
-    adScript.type = 'text/javascript';
-    adScript.src = '//servicefigured.com/' + adConfig.key + '/invoke.js';
-    document.body.appendChild(adScript);
-})();
+// Panggil fungsi loadAd setelah halaman selesai dimuat
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    loadAd();
+} else {
+    document.addEventListener('DOMContentLoaded', loadAd);
+}
