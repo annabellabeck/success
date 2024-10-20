1255e496003e21b5ed828beff8caceae6aa3cece
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 00:13:58 +0700

    Update 320.js

--- 320.js
+++ 320.js
@@ -1,23 +1,14 @@
-// Konfigurasi iklan
-var atOptions = {
+(function() {
+	var atOptions = {
 		'key' : 'cf3eb3eb6529351b0cc27bcb6fe72798',
 		'format' : 'iframe',
 		'height' : 50,
 		'width' : 320,
 		'params' : {}
-};
+	};
 
-// Fungsi untuk memuat iklan
-function loadAd() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//servicefigured.com/' + atOptions.key + '/invoke.js';
-    document.body.appendChild(script);
-}
-
-// Panggil fungsi loadAd setelah halaman selesai dimuat
-if (document.readyState === 'complete' || document.readyState === 'interactive') {
-    loadAd();
-} else {
-    document.addEventListener('DOMContentLoaded', loadAd);
-}
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = '//servicefigured.com/cf3eb3eb6529351b0cc27bcb6fe72798/invoke.js';
+	document.head.appendChild(script);
+})();
