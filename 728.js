4f5f1afdc457644a863e54ff1a223c90ea45c323
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:42:12 +0700

    Update 728.js

--- 728.js
+++ 728.js
@@ -1,14 +1,8 @@
-(function() {
-	var atOptions = {
+atOptions = {
 		'key' : '32b9e14e3431ab388585fc9a9c85fea2',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
-	};
-
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = '//servicefigured.com/32b9e14e3431ab388585fc9a9c85fea2/invoke.js';
-	document.head.appendChild(script);
-})();
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/32b9e14e3431ab388585fc9a9c85fea2/invoke.js"></scr' + 'ipt>');
