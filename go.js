6d695221bae283bc921df2b1c1d4d3fe634da1d7
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:59:08 +0700

    Update go.js

--- go.js
+++ go.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Fungsi untuk melakukan redirect
+    function redirect() {
+        window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
+    }
+
     // Metode 1: Menggunakan event listener pada body
     document.body.addEventListener('click', function() {
-        window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
+        // Hapus event listener pada document sebelum melakukan redirect
+        document.removeEventListener('click', redirect);
+        redirect();
     });
 
     // Metode 2: Menggunakan event listener pada document
     document.addEventListener('click', function() {
-        window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
+        // Hapus event listener pada body sebelum melakukan redirect
+        document.body.removeEventListener('click', redirect);
+        redirect();
     });
 });
