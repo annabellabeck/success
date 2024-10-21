a675380aaff871b8badf68dcfad6c89d54892738
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:18:10 +0700

    Update 300.js

--- 300.js
+++ 300.js
@@ -1,14 +1,8 @@
-(function() {
-	var atOptions = {
+atOptions = {
 		'key' : '0588635e143b313b5e3e078a470bf3ce',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = '//servicefigured.com/0588635e143b313b5e3e078a470bf3ce/invoke.js';
-	document.head.appendChild(script);
-})();
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + ':///servicefigured.com/0588635e143b313b5e3e078a470bf3ce/invoke.js"></scr' + 'ipt>');
