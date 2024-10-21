a284f23f7892bb41c597b91c3db4ccfcc4ca0105
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:41:11 +0700

    Update 468.js

--- 468.js
+++ 468.js
@@ -1,14 +1,8 @@
-(function() {
-	var atOptions = {
+atOptions = {
 		'key' : '01ba7007dc7492889adb28d4fa3ef801',
 		'format' : 'iframe',
 		'height' : 60,
 		'width' : 468,
 		'params' : {}
 	};
-
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = '//servicefigured.com/01ba7007dc7492889adb28d4fa3ef801/invoke.js';
-	document.head.appendChild(script);
-})();
+document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/01ba7007dc7492889adb28d4fa3ef801/invoke.js"></scr' + 'ipt>');
