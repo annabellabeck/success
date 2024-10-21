cc6908940e437151ab9a6f1df84e2ae4568be028
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:39:48 +0700

    Update 320.js

--- 320.js
+++ 320.js
@@ -1,14 +1,8 @@
-(function() {
-	var atOptions = {
+atOptions = {
 		'key' : 'cf3eb3eb6529351b0cc27bcb6fe72798',
 		'format' : 'iframe',
 		'height' : 50,
 		'width' : 320,
 		'params' : {}
-	};
-
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = '//servicefigured.com/cf3eb3eb6529351b0cc27bcb6fe72798/invoke.js';
-	document.head.appendChild(script);
-})();
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/cf3eb3eb6529351b0cc27bcb6fe72798/invoke.js"></scr' + 'ipt>');
