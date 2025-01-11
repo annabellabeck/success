1336e3a7906b0e3aed2ee65e78f5db8f52568d1b
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2025-01-11 22:25:27 +0700

    Update 320.js

--- 320.js
+++ 320.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'cf3eb3eb6529351b0cc27bcb6fe72798',
+		'key' : '7810e9e24de1b7058d1990b9e41d80f7',
 		'format' : 'iframe',
 		'height' : 50,
 		'width' : 320,
 		'params' : {}
 };
-document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/cf3eb3eb6529351b0cc27bcb6fe72798/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/7810e9e24de1b7058d1990b9e41d80f7/invoke.js"></scr' + 'ipt>');
