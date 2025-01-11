b643619329cab9acbdf404fca2eefb980cb3f0f0
Annabelle Beck <139564245+annabellabeck@users.noreply.github.com>
2025-01-11 22:06:15 +0700

    Update 300.js

--- 300.js
+++ 300.js
@@ -1,8 +1,12 @@
 atOptions = {
-    'key' : '0588635e143b313b5e3e078a470bf3ce',
+    'key' : '06cfb849d3d9a7a3644f2452cfe36d6b',
     'format' : 'iframe',
     'height' : 250,
     'width' : 300,
     'params' : {}
 };
-document.write('<scr' + 'ipt type="text/javascript" src="//servicefigured.com/0588635e143b313b5e3e078a470bf3ce/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/06cfb849d3d9a7a3644f2452cfe36d6b/invoke.js"></scr' + 'ipt>');
+
+
+
+
